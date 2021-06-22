32235941801b653af0e67c49f06ad1158a82be48
rohitrtk <rohit_rtk@hotmail.com>
2021-06-22 19:52:15 -0400

    Added checks for the json keys

--- test.rb
+++ test.rb
@@ -1,6 +1,13 @@
 require 'rest-client'
 require 'json'
 
+# Key name constants
+KEYS_NAME       = "name"
+KEYS_FAMILY     = "family"
+KEYS_GIVEN      = "given"
+KEYS_GENDER     = "gender"
+KEYS_BIRTHDATE  = "birthDate"
+
 response = RestClient.get("http://hapi.fhir.org/baseR4/Patient?_count=30&_pretty=true")
 
 ENTRIES = JSON.parse(response)["entry"]
@@ -8,18 +15,45 @@ num_entries = ENTRIES.length()
 
 ENTRY_RES = ENTRIES[0]["resource"]
 
+# Get id
 id = ENTRY_RES["id"]
 puts id
 
-last_name = ENTRY_RES["name"][0]["family"]
-first_name = ENTRY_RES["name"][0]["given"][0]
-puts(first_name + " " + last_name)
+# Checking for name information
+if ENTRY_RES.has_key? KEYS_NAME
+    if ENTRY_RES[KEYS_NAME][0].has_key? KEYS_FAMILY
+        last_name = ENTRY_RES[KEYS_NAME][0][KEYS_FAMILY]
+        puts last_name
+    else
+        puts "No last name information available."
+    end
+    
+    if ENTRY_RES[KEYS_NAME][0].has_key? KEYS_GIVEN
+        first_name = ENTRY_RES[KEYS_NAME][0][KEYS_GIVEN][0]
+        puts(first_name + " " + last_name)
+    else
+        puts "No first name information available."
+    end
 
-gender = ENTRY_RES["gender"]
-puts(gender)
+else
+    puts "No name information available."
+end
 
-birthdate = ENTRY_RES["birthDate"][0, 10]
-puts birthdate
+# Checking for gender information
+if ENTRY_RES.has_key? KEYS_GENDER
+    gender = ENTRY_RES[KEYS_GENDER]
+    puts(gender)
+else
+    puts "No gender information available."
+end
 
-age = `python agecalculator.py #{birthdate}`
-puts age
+# Checking for birthdate information
+if ENTRY_RES.has_key? KEYS_BIRTHDATE
+    birthdate = ENTRY_RES[KEYS_BIRTHDATE][0, 10]
+    puts birthdate
+
+    age = `python agecalculator.py #{birthdate}`
+    puts age
+else
+    puts "No birthdate information available."
+end
